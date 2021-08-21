9dc48e29260b5812175721932729e334e08b0773
heng1025 <472263356@qq.com>
2021-08-21 18:35:39 +0800

    chore: update env

--- src/index.tsx
+++ src/index.tsx
@@ -9,10 +9,10 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-const { REACT_APP_URL } = process.env;
+const { REACT_APP_API_URL } = process.env;
 
 const client = new ApolloClient({
-  uri: `${REACT_APP_URL}/graphql`,
+  uri: `${REACT_APP_API_URL}/graphql`,
   cache: new InMemoryCache(),
 });
 
